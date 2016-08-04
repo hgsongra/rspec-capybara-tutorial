ccb59e88fe67116b6f7bf6b1fdb2d3d1a713d085
Hitesh Songra <hgsongra@gmail.com>
2016-08-04 16:48:45 +0530

    first commit

---
+++ spec/features/create_article_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "Create article" do
+	scenario 'user create article' do
+		visit "/"	  
+		click_link "New Article"
+		fill_in "Title", with: "1st article"
+		fill_in "Body", with: "Whatever it is"
+		click_button "Create Article"
+		expect(page).to have_content("Article was successfully created.")
+		
+	end	
+end
