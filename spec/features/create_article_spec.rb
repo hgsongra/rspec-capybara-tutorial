9c11accba3af04d2eb8c3be4cf212962503a9b87
Hitesh Songra <hgsongra@gmail.com>
2016-08-05 12:58:24 +0530

    Add validation on Article

--- spec/features/create_article_spec.rb
+++ spec/features/create_article_spec.rb
@@ -9,4 +9,23 @@ RSpec.feature "Create article" do
 		click_button "Create Article"
 		expect(page).to have_content("Article was successfully created.")
 	end	
+
+	scenario 'validate article fields' do
+		visit "/"	  
+		click_link "New Article"
+		fill_in "Title", with: ""
+		fill_in "Body", with: ""
+		click_button "Create Article"
+		expect(page).to have_content("Title can't be blank")
+		expect(page).to have_content("Body can't be blank")
+	end
+
+	scenario 'validate articles title field' do
+		visit "/"	  
+		click_link "New Article"
+		fill_in "Title", with: ""
+		fill_in "Body", with: ""
+		click_button "Create Article"
+		expect(page).to have_content("Title can't be blank")
+	end
 end
