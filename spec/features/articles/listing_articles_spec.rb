8525ecf2fe1cb473d2d8a5b0037c7c734ab22b60
Hitesh Songra <hgsongra@gmail.com>
2016-08-16 16:09:36 +0530

    Restrict non-owner user to see the Edit and Delete links

--- spec/features/articles/listing_articles_spec.rb
+++ spec/features/articles/listing_articles_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.feature "Listing all articles"  do
 	before do
 		@hitesh = User.create!(email: 'hgsongra@gmail.com', password: 'Password')
-    login_as(@hitesh)
 		@article1 = Article.create(title: "1st Article Title", body: "1st Article Body", user: @hitesh)
 		@article2 = Article.create(title: "2nd Article Title", body: "2nd Article Body", user: @hitesh)
 	end	
@@ -14,5 +13,6 @@ RSpec.feature "Listing all articles"  do
 		expect(page).to have_content(@article1.body)
 		expect(page).to have_content(@article2.title)
 		expect(page).to have_content(@article2.body)
+		expect(page).not_to have_link("New Article")
 	end
 end
--- spec/features/articles/show_article_spec.rb
+++ spec/features/articles/show_article_spec.rb
@@ -1,17 +1,43 @@
 require "rails_helper"
 
-RSpec.feature "Listing all articles"  do
+RSpec.feature "Show details of article"  do
 	before do
 		@hitesh = User.create!(email: 'hgsongra@gmail.com', password: 'Password')
-    login_as(@hitesh)
+		@songra = User.create!(email: 'songra@gmail.com', password: 'Password')
 		@article = Article.create(title: "1st Article Title", body: "1st Article Body", user: @hitesh)
 	end	
 
-	scenario "List of articles" do
-		visit "/"
+	scenario "for non signed in user didn't see the Edit and Delete links" do
+		visit root_path
 		click_link @article.title
 		expect(page).to have_content(@article.title)
 		expect(page).to have_content(@article.body)
 		expect(current_path).to eq(article_path(@article))
+		expect(page).not_to have_link("Edit")
+		expect(page).not_to have_link("Delete")
 	end
+
+	scenario "for non-owner signed in user didn't see the Edit and Delete links" do
+		visit root_path
+		login_as(@songra)
+		click_link @article.title
+		expect(page).to have_content(@article.title)
+		expect(page).to have_content(@article.body)
+		expect(current_path).to eq(article_path(@article))
+		expect(page).not_to have_link("Edit")
+		expect(page).not_to have_link("Delete")
+	end
+
+	scenario "for signed in owner can see the Edit and Delete links" do
+		visit root_path
+		login_as(@hitesh)
+		click_link @article.title
+		expect(page).to have_content(@article.title)
+		expect(page).to have_content(@article.body)
+		expect(current_path).to eq(article_path(@article))
+		expect(page).to have_link("Edit")
+		expect(page).to have_link("Delete")
+	end
+
+
 end
