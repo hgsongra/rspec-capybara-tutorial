6dc21a43248cca60d52a9d8b78edf92c942531c6
Hitesh Songra <hgsongra@gmail.com>
2016-09-14 10:23:58 +0530

    Remove extra code

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -1,5 +1,4 @@
 def login_user(user)
-	puts "**************************"
 	allow(request.env['warden']).to receive(:authenticate!) { user }
 	allow(controller).to receive(:current_user) { user }
 end
